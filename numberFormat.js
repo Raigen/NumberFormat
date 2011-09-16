ee24e870cf4055e7aae42bc7ccbb6f59043152ae
Florian Oellerich <falkentavio@hotmail.de>
2011-09-16 14:27:48 +0200

    edited format

--- numberFormat.js
+++ numberFormat.js
@@ -1,28 +1,34 @@
 /**
  * @name            formatNumber
  * @description     formatting a number
+ * @author          Florian Oellerich
  * @param input     number to format
  * @param format    which format should the output have
  * @param thousand  what symbol for thousand separator
  * @param num       how many digits after separator
  * @returns {String}
  */
-function formatNumber(input, format, thousand, num) {
+function formatNumber(input, format, num) {
+  if (!input) {
+    input = '0.0';
+  }
   var numberString = '';
+  var firstNumber = '';
+  var secondNumber = '';
   var indexOfPoint = 0;
-  var firstNumber = 0;
-  var secondNumber = 0;
-  
+
+  format = format || '%1.%2';
+  num = num || 2;
+
   var result = '';
 
   //getting numbers
-  numberString = parseFloat(input).toFixed(num).toString();
+  numberString = parseFloat(input.replace(',','.')).toFixed(num).toString();
   indexOfPoint = numberString.indexOf('.');
-  firstNumber = parseInt(numberString.substring(0, indexOfPoint));
-  secondNumber = parseInt(numberString.substring(indexOfPoint + 1, numberString.lenght));
-  alert(firstNumber + '.' + secondNumber);
+  firstNumber = numberString.substring(0, indexOfPoint);
+  secondNumber = numberString.substring(indexOfPoint + 1, numberString.length + 1);
 
   //parsing format
-  result = String(format).replace('$1', firstNumber).replace('$2', secondNumber);
+  result = String(format).replace('%1', firstNumber).replace('%2', secondNumber);
   return result;
 }
